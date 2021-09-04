ae2cec6c44e87c99c1907c6bb6fdcd581790cb39
EugeneTerry <eugene.terry73@gmail.com>
2021-09-04 14:06:33 -0500

    first commit

---
+++ src/OTOI.js
@@ -0,0 +1,14 @@
+import React from "react"
+import {Route} from "react-router-dom"
+import {NavBar} from "./nav/NavBar"
+import {AppicationViews} from "./components/ApplicationViews"
+import "./components/OTOI.css"
+
+// export const OTOI =()=>(
+//   return (
+//   <>
+//     <Route>
+//       <NavBar/>
+//     </Route>
+//   </>)
+// )
---
+++ src/nav/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// export const NavBar =() =>{
+  
+//   return(
+//     <nav className="navbar navbar-light bg-light">
+//      OTOI
+//     </nav>
+//   )
+// }
