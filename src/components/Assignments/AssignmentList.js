22d2eac6b4c94dd9d39e5c692b7923cd4814083b
EugeneTerry <eugene.terry73@gmail.com>
2021-09-05 15:21:02 -0500

    added Assignment and user code

---
+++ src/components/Assignments/AssignmentList.js
@@ -0,0 +1,98 @@
+import React, {useEffect, useContext, useState} from "react";
+import { usehistory, Link} from "react-router-dom"
+import { AssignmentContext } from "./AssignmentProvider";
+import "./Assignment.css"
+
+export const AssignmentList = () => {
+  const {assignments, getAssignments, searchTerms} = useContext(AssignmentContext)
+  const currentUser = (parseInt(sessionStorage.getItem("otoi_user")))
+  const [setFiltered] = useState ([])
+  const history = usehistory()
+
+  useEffect(() => {
+    getAssignments()
+  }, [])
+
+  const filteredAssignments = assignments.filter(assignments => assignments.userId === currentUser)
+
+  const {setSearchTerms} = useContext(AssignmentContext)
+
+  useEffect(() => {setSearchTerms("")}, [])
+
+  useEffect(() => {
+    if(searchTerms !=="") {
+      const subset = assignments.filter(assignment => assignments.title.toLowerCase().includes(searchTerms.toLowerCase()) || assignments.classId.toLowerCase().includes(searchTerms.toLowerCase()) || assignments.classId.teacherId.toLowerCase().includes(searchTerms.toLowerCase()) || assignments.notes.toLowerCase().includes(searchTerms.toLowerCase()))
+      setFiltered(subset)
+    } else {
+      setFiltered(assignments)
+    }
+    }, [searchTerms, assignments])
+
+  return (
+    <>
+      <div className="assignmentDivList">
+        <section className="assignmentSectionList">
+          <div className="assignmentSearch">Assignments
+            <input type="text"
+            className="input--wide"
+            onKeyUp={(e) => setSearchTerms(e.currentTarget.value)}
+            placeholder="🔍" />
+          </div>
+          {filteredAssignments.map((assignment) => {
+            return (
+              <div className="linkDivAssignmentList"
+                key={`assignmentDivList=${assignment.id}`}>
+                <Link to={`/assignments/detail/${assignment.id}`}
+                  key={assignment.id}
+                  className="linkTitleAssignmentList">
+                  Title:{assignment.title}
+                </Link>
+                <div
+                  key={`assignmentClassList_${assignment.id}`}
+                  className="assignmentClassListInfo"
+                >
+                  Class: {assignment.classId.name}
+                </div>
+                <div
+                  key={`assignmentNotesList__${assignment.id}`}
+                  className="assignmentNotesListInfo"
+                >
+                  Notes: {assignment.notes}
+                </div>
+                <div
+                  key={`assignmentDateList__${assignment.id}`}
+                  className="assignmentDateListInfo"
+                >
+                  Date Assigned: {assignment.dateGiven}
+                </div>
+                <div
+                  key={`assignmentDueList__${assignment.id}`}
+                  className="assignmentDueListInfo"
+                >
+                  Date Due: {assignment.dateDue}
+                </div>
+                <div
+                  key={`assignmentStartedList__${assignment.id}`}
+                  className="assignmentStartedListInfo"
+                >
+                  Started: {assignment.started}
+                </div>
+                <div
+                  key={`assignmentFinishedList__${assignment.id}`}
+                  className="assignmentFinishedListInfo"
+                >
+                  Started: {assignment.finished}
+                </div>
+                
+              </div>
+            )
+          })}
+          <button className="assignmentBtn" onclick={() => history.push("/assignments/create")}></button>
+          Add New Assignment
+        </section>
+      </div>
+    </>
+  )
+
+
+}
---
+++ src/components/Assignments/AssignmentProvider.js
@@ -0,0 +1,67 @@
+import React, { useState, createContext } from "react"
+
+export const AssignmentContext = createContext()
+
+export const AssignmentProvider = (props) => {
+    const [assignments, setAssignments] = useState([])
+    const apiURL = "http://localhost:8088"
+   
+    const getAssignments = () => {
+        return fetch(`${apiURL}/assignments`)
+        .then(res => res.json())
+        .then(setAssignments)
+    }
+
+    const getAssignmentsByUserId = (userId) => {
+        return fetch(`${apiURL}/assignments/${userId}`)
+        .then(res => res.json())
+    }
+
+    const getAssignmentsByClassId = (classId) => {
+      return fetch(`${apiURL}/assignments/${classId}`)
+      .then(res => res.json())
+  }
+
+    const addAssignment = async assignment => {
+        const response = await fetch(`${apiURL}/assignments`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(assignment)
+      })
+      return await response.json()
+    }
+
+    const updateAssignment = assignmentToUpdate => {
+        return fetch((`${apiURL}/assignments/${assignmentToUpdate.id}`), {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(assignmentToUpdate)
+        })
+            .then(getAssignments)
+    }
+
+    const getAssignmentById = async (assignmentId) => {
+        const res = await fetch(`${apiURL}/assignments/${assignmentId}`)
+      return await res.json()
+    }
+
+    const deleteAssignment = (assignmentId) => {
+        return fetch((`${apiURL}/assignments/${assignmentId}`), {
+            method: "DELETE"
+        })
+        .then(getAssignments)
+    }
+
+
+    return (
+        <AssignmentContext.Provider value={{
+          assignments, getAssignments, addAssignment, getAssignmentsByUserId, updateAssignment, getAssignmentById, deleteAssignment, getAssignmentsByClassId
+        }}>
+            {props.children}
+        </AssignmentContext.Provider>
+    )
+}
